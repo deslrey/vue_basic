d9c5a28df19521383a79edd82205ab0858800055
1Deslre <2135232170@qq.com>
2024-06-03 23:57:24 +0800

    render函数

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -4,12 +4,18 @@
 // 引入 Vue
 import Vue from 'vue'
 // 引入 App 组件，它是所有组件的父组件
-import App from './App.vue'
+// import App from './App.vue'
 // 关闭 Vue 的生产提示
 Vue.config.productionTip = false
 
 // 创建 Vue 实例对象  --  vm
 new Vue({
+  el: '#app',
   // 将 App 组件放入容器中
   render: h => h(App),
-}).$mount('#app')
+
+  // render:q => q('h1','你好啊'),
+
+  // template: `<h1>你好啊</h1>`,
+  // components: { App }
+})
