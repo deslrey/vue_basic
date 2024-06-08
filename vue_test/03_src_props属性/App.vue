26617986e7fc5d5da483634b2e788356ecf0f426
1Deslre <2135232170@qq.com>
2024-06-09 00:09:07 +0800

    props属性

---
+++ vue_test/03_src_props属性/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Student name="张三" sex="男" :age="19" />
+    </div>
+</template>
+
+<script>
+
+import Student from './components/Student';
+
+export default {
+    name: 'App',
+    components: { Student }
+}
+</script>
+
+<style></style>
