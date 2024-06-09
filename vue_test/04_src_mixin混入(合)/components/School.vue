a82d8f3fd594f4010d06c57a67dfdc8aea359d1f
1Deslre <2135232170@qq.com>
2024-06-09 19:04:50 +0800

    mixin混合

---
+++ vue_test/04_src_mixin混入(合)/components/School.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2 @click="showName">学校名称 : {{ name }}</h2>
+        <h2>学校地址 : {{ address }}</h2>
+    </div>
+</template>
+
+<script>
+//  引入混合
+// import { mixin, hunhe2 } from '../js/minin'
+
+export default {
+    name: 'School',
+    data() {
+        return {
+            name: "尚硅谷",
+            address: '武汉'
+        }
+    },
+    // mixins: [mixin, hunhe2]
+}
+</script>
