ba028b96ffa8158b5279cfad41038a21aba5b7e0
1Deslre <2135232170@qq.com>
2024-06-10 00:17:10 +0800

    TodoList案例_静态

---
+++ vue_test/src/components/List.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="todo-main">
+        <MyItem />
+    </ul>
+</template>
+
+<script>
+import MyItem from '../components/MyItem'
+
+export default {
+    name: 'List',
+
+    components: { MyItem }
+}
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+}
+
+.todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+}
+</style>
