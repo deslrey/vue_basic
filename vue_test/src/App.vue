ce61bbf1042c06e8b1681e4dcbf495cbd150f3cb
1Deslre <2135232170@qq.com>
2024-06-17 22:38:05 +0800

    组件自定义事件_绑定

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,108 +1,47 @@
 <template>
-    <div id="root">
-        <div class="todo-container">
-            <div class="todo-wrap">
-                <MyHeader :addTodo="addTodo" />
-                <List :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo" />
-                <MyFooter :todos="todos" :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo" />
-            </div>
-        </div>
+    <div class="app">
+        <h1>{{ msg }}</h1>
+        <!-- 通过父组件给子组件传递函数类型的 props 实现：子给父传递数据 -->
+        <School :getSchoolName="getSchoolName" />
+        <hr />
+        <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据(第一种写法，使用 @ 或 v-on) -->
+        <!-- <Student v-on:atguigu="getStudentName" /> -->
+        <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据(第一种写法，使用 ref) -->
+        <Student ref="student" />
     </div>
 </template>
 
 <script>
 
-import List from './components/List';
-import MyHeader from './components/MyHeader';
-import MyFooter from './components/MyFooter';
+import School from './components/School';
+import Student from './components/Student';
 
 export default {
     name: 'App',
-    components: { List, MyHeader, MyFooter },
+    components: { School, Student },
     data() {
         return {
-            todos: JSON.parse(localStorage.getItem('todos')) || []
+            msg: '你好啊'
         }
     },
     methods: {
-        // 添加一个 todo
-        addTodo(todoObj) {
-            this.todos.unshift(todoObj)
+        getSchoolName(name) {
+            console.log('SchoolName = ', name);
         },
-        // 勾选 or 取消勾选一个 todo
-        checkTodo(id) {
-            this.todos.forEach((todo) => {
-                if (todo.id === id) todo.done = !todo.done
-            })
-        },
-        // 删除一个 todo
-        deleteTodo(id) {
-            this.todos = this.todos.filter(todo => todo.id !== id)
-        },
-        // 全选 or 取消全选
-        checkAllTodo(done) {
-            this.todos.forEach((todo) => todo.done = done)
-        },
-        // 清除所有已完成的任务
-        clearAllTodo() {
-            this.todos = this.todos.filter((todo) => {
-                return !todo.done
-            })
+        getStudentName(name) {
+            console.log('StudentName = ', name);
         }
     },
-    watch: {
-        todos: {
-            deep: true,
-            handler(value) {
-                localStorage.setItem('todos', JSON.stringify(value))
-            }
-        }
+    mounted() {
+        // this.$refs.student.$on('atguigu', this.getStudentName)
+        this.$refs.student.$once('atguigu', this.getStudentName)
     }
 }
 </script>
 
 <style>
-/*base*/
-body {
-    background: #fff;
-}
-
-.btn {
-    /* display: inline-block; */
-    padding: 4px 12px;
-    margin-bottom: 0;
-    font-size: 14px;
-    line-height: 20px;
-    text-align: center;
-    vertical-align: middle;
-    cursor: pointer;
-    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-    border-radius: 4px;
-}
-
-.btn-danger {
-    color: #fff;
-    background-color: #da4f49;
-    border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-    color: #fff;
-    background-color: #bd362f;
-}
-
-.btn:focus {
-    outline: none;
-}
-
-.todo-container {
-    width: 600px;
-    margin: 0 auto;
-}
-
-.todo-container .todo-wrap {
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
+.app {
+    background-color: gray;
+    padding: 5px;
 }
 </style>
---
+++ vue_test/src/components/Student.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="student">
+        <h2>学生姓名 : {{ name }}</h2>
+        <h2>学生性别 : {{ sex }}</h2>
+        <button @click="sendStudentName">点我发送</button>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'Student',
+
+    data() {
+        return {
+            name: "张三",
+            sex: '男'
+        }
+    },
+    methods: {
+        sendStudentName() {
+            this.$emit('atguigu', this.name)
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.student {
+    background-color: greenyellow;
+    padding: 5px;
+    margin-top: 30px;
+}
+</style>
