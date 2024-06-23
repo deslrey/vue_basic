4172a72a19b5941a2d3c5069f59aaf9694e36b10
1Deslre <2135232170@qq.com>
2024-06-23 15:54:56 +0800

    动画效果和过度效果

---
+++ vue_test/src/components/Test2.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <button @click="isShow = !isShow">显示/隐藏</button>
+        <transition name="hello" appear>
+            <h1 v-show="isShow">你好啊</h1>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test2',
+    data() {
+        return {
+            isShow: true
+        }
+    },
+}
+</script>
+
+<style scoped>
+h1 {
+    background-color: aqua;
+
+}
+
+.hello-enter,
+.hello-leave-to {
+    transform: translateX(-100%);
+}
+
+.hello-enter-active,
+.hello-leave-active {
+    transition: 1s linear;
+}
+
+.hello-leave,
+.hello-enter-to {
+    transform: translateX(0px);
+}
+</style>
