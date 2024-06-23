63cbd8d2bf83d558cf1fdd00adbcea3001938aa6
1Deslre <2135232170@qq.com>
2024-06-23 20:38:29 +0800

    配置代理服务器

--- vue_test/vue.config.js
+++ vue_test/vue.config.js
@@ -5,5 +5,26 @@ module.exports = {
       entry: 'src/main.js',
     },
   },
-  lintOnSave: false
+  lintOnSave: false,
+  // 开启代理服务器（方式一）
+  // devServer: {
+  //   proxy: 'http://localhost:5000'
+  // }
+  // 开启代理服务器（方式二）
+  devServer: {
+    proxy: {
+      '/atguigu': {
+        target: 'http://localhost:5000',
+        pathRewrite: { '^/atguigu': '' },
+        // ws: true,  //  用于支持 websocket
+        // changeOrigin: true //  用于控制请求头中的 host 值
+      },
+      '/demo': {
+        target: 'http://localhost:5001',
+        pathRewrite: { '^/demo': '' },
+        // ws: true,  //  用于支持 websocket
+        // changeOrigin: true //  用于控制请求头中的 host 值
+      }
+    }
+  }
 }
