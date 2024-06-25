8607bd8d7db9e7fad0e7b2c91f15c481e5d3160c
1Deslre <2135232170@qq.com>
2024-06-25 23:30:15 +0800

    GitHub案例_vue-resource

---
+++ vue_test/21_src_github案例_vue-resource/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="container">
+		<Search />
+		<List />
+	</div>
+</template>
+
+<script>
+
+import List from "./components/List";
+import Search from "./components/Search";
+
+export default {
+	name: 'App',
+	components: { Search, List }
+
+}
+</script>
