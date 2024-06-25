5bd8b97b3e00301a9ec106642c68866d21b1301d
1Deslre <2135232170@qq.com>
2024-06-25 23:18:21 +0800

    GitHub案例

---
+++ vue_test/src/components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="jumbotron">
+        <h3 class="jumbotron-heading">Search Github Users</h3>
+        <div>
+            <input type="text" placeholder="enter the name you search" v-model="keyWord" />
+            <button @click="searchUsers">Search</button>
+        </div>
+    </section>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    name: 'Search',
+    data() {
+        return {
+            keyWord: ''
+        }
+    },
+    methods: {
+        searchUsers() {
+            this.$bus.$emit('updateListData', { isFirst: false, isLoading: true, errMsg: '', users: [] })
+            axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+                response => {
+                    console.log('请求成功');
+                    this.$bus.$emit('updateListData', { isLoading: false, errMsg: '', users: response.data.items })
+                },
+                error => {
+                    this.$bus.$emit('updateListData', { isLoading: false, errMsg: error.message, users: [] })
+                }
+            )
+        }
+    }
+}
+</script>
