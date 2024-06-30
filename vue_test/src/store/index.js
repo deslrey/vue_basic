94a5426adb818af761b52bcf31207c84518f7a8b
1Deslre <2135232170@qq.com>
2024-06-30 21:31:21 +0800

    求和案例vuex版

---
+++ vue_test/src/store/index.js
@@ -0,0 +1,51 @@
+// 该文件由于创建Vuex中最为核心的 store
+import Vue from 'vue'
+// 引入 Vuex
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+// 准备actions - 用于响应组件中的动作
+const actions = {
+    // jia(context, value) {
+    //     console.log('actions 中的 jia 被调用了')
+    //     context.commit('JIA', value)
+    // },
+    // jian(context, value) {
+    //     console.log('actions 中的 jian 被调用了')
+    //     context.commit('JIAN', value)
+    // }
+    jiaOdd(context, value) {
+        if (context.state.sum % 2) {
+            console.log('actions 中的 jiaOdd 被调用了')
+            context.commit('JIA', value)
+        }
+    },
+    jiaWait(context, value) {
+        setTimeout(() => {
+            console.log('actions 中的 jiaWait 被调用了')
+            context.commit('JIA', value)
+        }, 500)
+    }
+}
+// 准备mutations - 用于操作数据（state）
+const mutations = {
+    JIA(state, value) {
+        console.log('mutations 中的 JIA 被调用了')
+        state.sum += value
+    },
+    JIAN(state, value) {
+        console.log('mutations 中的 JIAN 被调用了')
+        state.sum -= value
+    }
+}
+// 准备state - 用于存储数据
+const state = {
+    sum: 0
+}
+
+// 创建并暴露 state
+export default new Vuex.Store({
+    actions,
+    mutations,
+    state
+})
