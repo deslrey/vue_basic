c68fe18a614ecd52c9009d6ac7e3db04e236a7c9
1Deslre <2135232170@qq.com>
2024-07-07 00:15:19 +0800

    两个新的生命周期钩子

---
+++ vue_test/38_src_两个新的生命周期钩子/router/index.js
@@ -0,0 +1,50 @@
+import VueRouter from "vue-router"
+
+import About from "../pages/About"
+import Home from "../pages/Home"
+import News from "../pages/News"
+import Message from "../pages/Message"
+import Detail from "../pages/Detail"
+
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '/about',
+            component: About
+        },
+        {
+            path: '/home',
+            component: Home,
+            children: [
+                {
+                    path: 'news',
+                    component: News
+                }, {
+                    path: 'message',
+                    component: Message,
+                    children: [
+                        {
+                            name: 'detail',
+                            path: 'detail',
+                            component: Detail,
+                            // props 的第一种写法,值为对象,该对象中的所有key-value都会以props的形式传给Detail组件
+                            // props: { a: 1, b: 'hello' }
+
+                            // props 的第二种写法,值为布尔值,若布尔值为真,就会把该路由组件收到的所有params参数,以props的形式传给Detail组件
+                            // props: true
+
+                            // props 的第三种写法,值为函数
+                            props($route) {
+                                return {
+                                    id: $route.query.id,
+                                    title: $route.query.title
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+})
