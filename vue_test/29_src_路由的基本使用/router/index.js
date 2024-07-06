f39ef819ea8e38410b90ab17bd4cc4e677be22d2
1Deslre <2135232170@qq.com>
2024-07-06 16:47:54 +0800

    路由的基本使用

---
+++ vue_test/29_src_路由的基本使用/router/index.js
@@ -0,0 +1,18 @@
+import VueRouter from "vue-router"
+
+import About from "../components/About"
+import Home from "../components/Home"
+
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '/about',
+            component: About
+        },
+        {
+            path: '/home',
+            component: Home
+        }
+    ]
+})
