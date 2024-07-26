848c9da7b3f0fcb52ed51f184a5a1aef2af6277e
deslre <2135232170@qq.com>
2024-07-26 23:59:28 +0800

    Vue3的响应式原理

--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>我是App组件</h1>
-  <h1>姓名: {{ person.name }}</h1>
+  <h1 v-show="person.name">姓名: {{ person.name }}</h1>
   <h1>年龄: {{ person.age }}</h1>
+  <h1 v-show="person.sex">性别: {{ person.sex }}</h1>
   <h1>工作种类: {{ person.job.type }}</h1>
   <h1>工作薪水: {{ person.job.salary }}</h1>
   <h1>测试的数据: {{ person.job.a.b.c }}</h1>
   <h1>爱好: {{ person.hobby }}</h1>
   <button @click="changeInfo">点我修改数据</button>
+  <button @click="addSex">添加一个sex属性</button>
+  <button @click="deleteName">删除一个name属性</button>
 </template>
 
 <script>
@@ -40,9 +43,18 @@ export default {
       person.job.a.b.c = 999
     }
 
+    function addSex() {
+      person.sex = '男'
+    }
+    function deleteName() {
+      delete person.name
+    }
+
     return {
       person,
-      changeInfo
+      changeInfo,
+      addSex,
+      deleteName
     }
   }
 }
