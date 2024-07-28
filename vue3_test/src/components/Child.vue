3f45642a1500a2b0ff08b2e274deaee88f517fce
deslre <2135232170@qq.com>
2024-07-28 16:04:32 +0800

    teleport

---
+++ vue3_test/src/components/Child.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="chlid">
+        <h3>我是Chlid组件</h3>
+        <Son />
+    </div>
+
+
+</template>
+
+<script>
+import Son from './Son.vue';
+
+export default {
+    name: 'Chlid',
+    components: { Son }
+
+}
+</script>
+
+<style scoped>
+.chlid {
+    background-color: orange;
+    padding: 10px;
+}
+</style>
